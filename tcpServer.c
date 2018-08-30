002c4d72b9f76d52abb31a3cacf1173f7400f909
Alexander Protopopov <42470635+Sashko30208@users.noreply.github.com>
2018-08-31 01:56:59 +0700

    createsetvtsp

---
+++ tcpServer.c
@@ -0,0 +1,97 @@
+/* Creating a simple UDP server with main functions*/
+#include <stdio.h>
+#include <string.h> //memset()
+#include <stdlib.h>  //exit()
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include <unistd.h>//close()
+
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#define BUFLEN 512 //max Buffer length
+#define PORT 8888
+#define REPLY " Is_Reply\n"
+
+void die (char *s)
+{
+perror(s);
+exit(1);
+}
+
+//char mess_serv[] = "\nHello client!";
+int main()
+{
+	/*
+	 * listener для хранения fd сокета
+	 */
+	int fd_s, listening;
+	struct sockaddr_in si;
+	char buf[BUFLEN];
+	int recv_len;
+/*(AF_INET(internet-домен),SOCK_STREAM(для TCP),протокол по умолчанию)*/
+	if(listening = socket(AF_INET, SOCK_STREAM, 0)==-1) //создание сокета
+	{
+        die("socket()");
+    }
+	//проверка на создание сокета, если listening -1 то не создался сокет
+
+	
+	/*заполнение сткутуры*/
+	
+	si.sin_family = AF_INET; //семейство адресов
+	
+	//Host TO Network Short - преобразование числа порядка хоста в сетевой
+	si.sin_port = htons(PORT); //номер порта
+	
+	//Host TO Network Long - реобразование числа порядка хоста в сетевой
+	// INADDR_ANY - соединения с клиентами через любой IP
+	addr.sin_addr.s_addr = htonl(INADDR_ANY); //IP-адрес
+	
+	//bind - для явного связывание с некоторым адресом
+	if(bind(listening, (struct sockaddr *)&addr, sizeof(addr)) ==-1) 
+    {
+		die("bind");
+	}
+	
+	/*очередь запросов на соединение
+	 * первый параметр это fd сокета
+	 * второй это размер очереди запросов
+	 */
+	 
+	listen(listening, 1);
+	while(1) {
+		//accept - создает для общение с клиентом новый сокет
+		
+		if((fd_s = accept(listening, NULL, NULL)) < 0)
+        {
+			die("accept");
+		}
+		
+		while(1) {
+			
+			//recv - принимает данные
+			;
+			if((recv_len = recv(fd_s, buf, BUFLEN, 0)) <= 0) 
+            {
+                break;
+            }
+
+            if (sizeof(str)<BUFLEN)
+            {
+                sprintf(*(&buf), "%s%s\n",buf, REPLY);
+                printf("%d\n",strlen(buf));
+            }
+            //strcat(buf, mess_serv);
+			//send для отправки данных 
+			if (send(fd_s, buf, 2*recv_len, 0)== -1)
+            {
+                die("sendto()");
+            }
+		}
+		
+		close(fd_s);
+	}
+	return 0;
+}
